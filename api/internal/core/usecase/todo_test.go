99a0f1799ac583ba5dace0737845ec39b5007278
Sajjad Pouladvand <sajjadpld1998@gmail.com>
2025-11-23 14:59:10 +0330

    changes

--- api/internal/core/usecase/todo_test.go
+++ api/internal/core/usecase/todo_test.go
@@ -2,12 +2,7 @@ package usecase
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/mock/gomock"
-	"go.uber.org/zap"
 	localeMock "microservice/internal/adapter/locale/mocks"
 	loggerMock "microservice/internal/adapter/logger/mocks"
 	"microservice/internal/core/domain"
@@ -15,6 +10,10 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
 )
 
 func TestTodoUsecase_Create(t *testing.T) {
@@ -136,79 +135,5 @@ func TestTodoUsecase_Create(t *testing.T) {
 		assert.Error(t, err)
 		assert.Equal(t, expectedErr, err)
 		assert.Nil(t, result)
-	})
-
-	t.Run("todo queue error", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		//
-
-		locale := localeMock.NewMockILocale(ctrl)
-		logger := loggerMock.NewMockILogger(ctrl)
-		todoRepo := todoRepoMock.NewMockITodoRepository(ctrl)
-
-		//
-
-		uc := NewTodo(logger, locale, todoRepo)
-
-		//
-
-		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-		defer cancel()
-
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-
-		queueErr := errors.New("queue consume failure")
-
-		todoRepo.EXPECT().Create(ctx, testTodo).Return(expectedTodo, nil).Times(1)
-
-		logger.EXPECT().Error("todo.uc.create.queue.send", gomock.Any()).DoAndReturn(func(scope string, fields ...interface{}) {
-			errorFound := false
-			errorItemIdFound := false
-
-			for _, field := range fields {
-				if zapField, ok := field.(zap.Field); ok {
-					switch zapField.Key {
-					case "error":
-						if zapField.Interface != nil {
-							if err, ok := zapField.Interface.(error); ok {
-								assert.Equal(t, queueErr.Error(), err.Error(), "expected queue error")
-								errorFound = true
-							}
-						}
-					case "item.id":
-						if zapField.String != "" {
-							assert.Equal(t, expectedTodo.UUID().String(), zapField.String, "expected specific queue item id")
-							errorItemIdFound = true
-						}
-					}
-				}
-			}
-
-			assert.True(t, errorFound, "expected error found in log")
-			assert.True(t, errorItemIdFound, "expected item id found in log")
-		}).Times(1)
-
-		result, err := uc.Create(ctx, testTodo)
-
-		assert.NoError(t, err)
-		assert.Equal(t, nil, err)
-		assert.Equal(t, expectedTodo.UUID(), result.UUID())
-
-		// wait for goroutine used in `Create` method to complete with timeout
-		done := make(chan struct{})
-		go func() {
-			wg.Wait()
-			close(done)
-		}()
-
-		select {
-		case <-done:
-			t.Log("goroutine completed")
-		case <-time.After(3 * time.Second):
-			t.Fatal("goroutine timeout")
-		}
 	})
 }
